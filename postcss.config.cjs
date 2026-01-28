47c444a05ff2f5985d016deb8710e45b160672be
diego.rapichan <direrapichan@gmail.com>
2026-01-28 10:16:58 -0300

    vercel

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,8 +3,5 @@ const tailwindPostcss = require("@tailwindcss/postcss");
 const autoprefixer = require("autoprefixer");
 
 module.exports = {
-  plugins: {
-    tailwindcss: tailwindPostcss(),
-    autoprefixer: autoprefixer(),
-  },
+  plugins: [tailwindPostcss(), autoprefixer()],
 };
